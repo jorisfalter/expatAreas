ce796893aee15677bfd58a72aa176808c79aa023
Joris Falter <joris@jorisfalter.com>
2023-11-14 14:21:00 +0700

    x

--- public/index.js
+++ public/index.js
@@ -63,55 +63,6 @@ const regions = [
   // ... Add more regions
 ];
 
-// map.on("load", function () {
-//   // Add a new source from our GeoJSON data
-//   map.addSource("algarve", {
-//     type: "geojson",
-//     data: algarveGeoJSON,
-//   });
-//   // Add a new layer to visualize the polygon.
-//   map.addLayer({
-//     id: "algarve-layer",
-//     type: "fill",
-//     source: "algarve", // reference the data source
-//     layout: {},
-//     paint: {
-//       "fill-color": "#0000FF", // yellow color
-//       "fill-opacity": 0.6,
-//     },
-//   });
-
-//   // When the user moves their mouse over the Algarve layer, we'll update the tooltip to display
-//   map.on("mouseenter", "algarve-layer", function (e) {
-//     // Change the cursor style as a UI indicator.
-//     map.getCanvas().style.cursor = "pointer";
-
-//     // Populate and display the tooltip. We'll use the coordinates of the mouse position.
-//     var coordinates = e.lngLat;
-//     var description =
-//       "Cost of living: medium<br>Population: 500,000 inhabitants<br>Population in high season: <br>Average population age: 45<br>Avg hottest month temp: 29 degrees celsius<br>Avg coldest month temp: 16.7 degrees celsius<br>Number of international schools: 13";
-//     // Ensure that if the map is zoomed out such that multiple copies of the feature are visible,
-//     // the tooltip appears over the copy being pointed to.
-//     while (Math.abs(e.lngLat.lng - coordinates.lng) > 180) {
-//       coordinates.lng += e.lngLat.lng > coordinates.lng ? 360 : -360;
-//     }
-
-//     // Set the tooltip content and display it
-//     var tooltip = document.getElementById("tooltip");
-//     tooltip.innerHTML = description;
-//     tooltip.style.display = "block";
-//     tooltip.style.left = e.point.x + "px";
-//     tooltip.style.top = e.point.y + "px";
-//   });
-
-//   // Create a mouseout event that hides the tooltip when the user's mouse leaves the layer.
-//   map.on("mouseleave", "algarve-layer", function () {
-//     map.getCanvas().style.cursor = "";
-//     var tooltip = document.getElementById("tooltip");
-//     tooltip.style.display = "none";
-//   });
-// });
-
 map.on("load", function () {
   regions.forEach((region) => {
     // Add source for each region
