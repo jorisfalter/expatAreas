0185cd2f9da51546439f3472c1906096e0883d62
Joris Falter <joris@jorisfalter.com>
2024-01-08 11:05:17 +0700

    x

--- createOval.js
+++ createOval.js
@@ -20,12 +20,39 @@ let diff_array_y = [
   y1 - start_array[3][1],
 ];
 
+console.log(diff_array_x);
+console.log(diff_array_y);
+
 // Make it positive
 let positive_x = diff_array_x.map((value) => Math.abs(value));
 let positive_y = diff_array_y.map((value) => Math.abs(value));
 
 let x_length = Math.max(...positive_x);
 let y_length = Math.max(...positive_y);
+let x_maxIndex = positive_x.indexOf(x_length); // Find the index of the max value
+let y_maxIndex = positive_y.indexOf(y_length); // Find the index of the max value
 
 console.log(x_length);
 console.log(y_length);
+console.log(x_maxIndex);
+console.log(y_maxIndex);
+
+// I'm indifferent between long and short axis, I'm not sure if this is a problem
+let a_axis = (x_length * 1.2) / 2;
+let b_axis = (y_length * 1.2) / 2;
+
+// Now I need to find the middle point
+// I know the first coordinate
+// I just need to know the second
+// if maxIndex is 0, it means the second coordinate, if maxIndex is 1, it means the third coordinate
+
+let x2 = start_array[x_maxIndex + 1][0];
+let y2 = start_array[y_maxIndex + 1][1];
+
+let Cx = x2 + (x1 - x2) / 2;
+let Cy = y2 + (y1 - y2) / 2;
+
+console.log(x2);
+console.log(y2);
+console.log(Cx);
+console.log(Cy);
