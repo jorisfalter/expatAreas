d83ccea164c891ae5db74f5ccfe5300e2707353b
Joris Falter <joris@jorisfalter.com>
2024-01-08 11:24:26 +0700

    x

--- createOval.js
+++ createOval.js
@@ -1,9 +1,17 @@
+// let start_array = [
+//   [-8.995264387023342, 37.52613001504033],
+//   [-8.995264387023342, 37.027291043969406],
+//   [-7.513023249060555, 37.027291043969406],
+//   [-7.513023249060555, 37.52613001504033],
+//   [-8.995264387023342, 37.52613001504033],
+// ];
+
 let start_array = [
-  [-8.995264387023342, 37.52613001504033],
-  [-8.995264387023342, 37.027291043969406],
-  [-7.513023249060555, 37.027291043969406],
-  [-7.513023249060555, 37.52613001504033],
-  [-8.995264387023342, 37.52613001504033],
+  [-2, -1],
+  [-2, 1],
+  [2, 1],
+  [2, -1],
+  [-2, -1],
 ];
 
 // find the x and y distance between coordinates. Because I don't always use the same sequence (eg clockwise coords) I have to iteratively find the coordinates
@@ -32,14 +40,12 @@ let y_length = Math.max(...positive_y);
 let x_maxIndex = positive_x.indexOf(x_length); // Find the index of the max value
 let y_maxIndex = positive_y.indexOf(y_length); // Find the index of the max value
 
-console.log(x_length);
-console.log(y_length);
-console.log(x_maxIndex);
-console.log(y_maxIndex);
-
 // I'm indifferent between long and short axis, I'm not sure if this is a problem
 let a_axis = (x_length * 1.2) / 2;
 let b_axis = (y_length * 1.2) / 2;
+
+console.log("a_axis " + a_axis);
+console.log("b_axis " + b_axis);
 
 // Now I need to find the middle point
 // I know the first coordinate
@@ -56,3 +62,33 @@ console.log(x2);
 console.log(y2);
 console.log(Cx);
 console.log(Cy);
+
+// I need to prepare a list of coordinates which I want to solve for y
+let x_array_zero_left = Cx - a_axis * 1.2; // should return zero for y
+let x_array_zero_right = Cx + a_axis * 1.2; // should return zero for y
+
+// this is the array of points we will find y coords for
+let x_array = [x_array_zero_left, Cx, x_array_zero_right];
+let coordinates_pairs_pos = [];
+let coordinates_pairs_neg = [];
+
+// Now we have the center points (Cx and Cy) and the long and short axis a and b
+// We can calculate coordinates
+// for (let i = 0; i < x_array.length; i++) {
+let i = 0;
+let num = x_array[i] - Cx;
+console.log(num);
+let denom = a_axis ** 2;
+console.log(denom);
+
+let fract = num / denom;
+let below_sqrt = 1 - fract;
+let b_sqrt = b_axis * Math.sqrt(below_sqrt);
+let y_pos = Cy + b_sqrt;
+let y_neg = Cy - b_sqrt;
+coordinates_pairs_pos.push([x_array[i], y_pos]);
+coordinates_pairs_neg.push([x_array[i], y_neg]);
+// }
+
+console.log(coordinates_pairs_pos);
+console.log(coordinates_pairs_neg);
